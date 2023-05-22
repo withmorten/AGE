65ea93992be157a3326f0837666d8582b0f9b117
withmorten <morten.with@gmail.com>
2023-05-22 23:10:22 +0200

    Show all angles of DE sprites. Fixed targeting with mouse as well.

--- AGE_Frame/Other.cpp
+++ AGE_Frame/Other.cpp
@@ -277,6 +277,9 @@ void AGE_Frame::OnOpen(wxCommandEvent&)
     }
     else wxMessageBox("No file " + DatFileName);
 
+    FillMissingFrames = GenieVersion > genie::GV_LatestTap && GenieVersion < genie::GV_C2 ||
+        GenieVersion > genie::GV_LatestDE2 || GenieVersion < genie::GV_Tapsa;
+
     // txt language file
     LangTxt.clear();
     LangIni.clear();
@@ -4863,8 +4866,14 @@ void AGE_Frame::OnFrameMouse(wxMouseEvent &event)
     wxPoint coords(slp_view->ScreenToClient(wxGetMousePosition()));
     coords.x -= centerX;
     coords.y -= centerY;
-    bearing = atan2(coords.x, -coords.y << 1) + 3.1416f;
-    assert(bearing >= 0.f && PI2 >= bearing);
+    float anchor = FillMissingFrames ? 3.1416f : 4.7124f;
+    bearing = atan2(coords.x, -coords.y << 1) + anchor;
+    if (bearing >= PI2A)
+    {
+        assert(!FillMissingFrames);
+        bearing -= PI2A;
+    }
+    assert(bearing >= 0.f && PI2 > bearing);
     setbearing = 1u;
     slp_view->Refresh();
 }
